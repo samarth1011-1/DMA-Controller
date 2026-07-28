49b7e2d912915778901e7627c7d18001ba2b4112
Shreenidhi V <104721161+shreenxdhi@users.noreply.github.com>
2026-07-28 22:57:04 +0530

    changes

--- create_zynq_project.tcl
+++ create_zynq_project.tcl
@@ -16,11 +16,15 @@ create_bd_cell -type module -reference dma_controller_dual dma_0
 connect_bd_intf_net [get_bd_intf_pins dma_0/m_axis] [get_bd_intf_pins dma_0/s_axis]
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Slave "/ps7_0/S_AXI_HP0" intc_ip "Auto" Master "/dma_0/m_axi" clk "Auto" } [get_bd_intf_pins ps7_0/S_AXI_HP0]
 apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config { Master "/ps7_0/M_AXI_GP0" intc_ip "Auto" Slave "/dma_0/s_axi" clk "Auto" } [get_bd_intf_pins dma_0/s_axi]
+set rst_pin [get_bd_pins -of_objects [get_bd_cells -filter {VLNV =~ "*proc_sys_reset*"}] -filter {NAME == "peripheral_aresetn"}]
+if {$rst_pin ne ""} {
+    connect_bd_net $rst_pin [get_bd_pins dma_0/rst_n]
+}
 assign_bd_address
 validate_bd_design
 save_bd_design
 make_wrapper -files [get_files ./vivado_blackboard_hw/dma_blackboard_hw.srcs/sources_1/bd/zynq_dma_bd/zynq_dma_bd.bd] -top
-add_files -norecurse ./vivado_blackboard_hw/dma_blackboard_hw.srcs/sources_1/bd/zynq_dma_bd/hdl/zynq_dma_bd_wrapper.v
+add_files -norecurse ./vivado_blackboard_hw/dma_blackboard_hw.gen/sources_1/bd/zynq_dma_bd/hdl/zynq_dma_bd_wrapper.v
 set_property top zynq_dma_bd_wrapper [current_fileset]
 puts "vivado project created"
 puts "project name : $project_name"
